51534d9b27dd4fda252191029715107aee102505
Peter Szabados <kangirigungi@gmail.com>
2016-10-23 16:37:28 +0200

    It loads

---
+++ MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SASHotkeys
+{
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class SASHotkeysMod : MonoBehaviour
+	{
+		void Awake()
+		{
+			Debug.Log ("<<<< Awaking SasHotkeys mod >>>>");
+		}
+	}
+}

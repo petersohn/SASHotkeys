4f79aa61802c3237a03e369019b6d7e560c86fdd
Peter Szabados <kangirigungi@gmail.com>
2016-11-30 21:57:13 +0100

    Make GUI buttons sized the same

--- SpaceCentreBehaviour.cs
+++ SpaceCentreBehaviour.cs
@@ -59,7 +59,7 @@ namespace SASHotkeys
 				GUILayout.BeginHorizontal ();
 				GUILayout.Label (element.Key);
 				DrawSelectorButton (element.Value);
-				if (GUILayout.Button ("Clear")) {
+				if (GUILayout.Button (clearButtonText, ClearButtonWidth)) {
 					element.Value.KeyBinding = new KeyBinding ();
 				}
 				GUILayout.EndHorizontal ();
@@ -71,11 +71,11 @@ namespace SASHotkeys
 		private void DrawSelectorButton(HotkeyAction hotkeyAction)
 		{
 			if (currentAction == hotkeyAction) {
-				if (GUILayout.Button ("...")) {
+				if (GUILayout.Button ("...", HotkeyButtonWidth)) {
 					currentAction = null;
 				}
 			} else {
-				if (GUILayout.Button (hotkeyAction.KeyBinding.name)) {
+				if (GUILayout.Button (hotkeyAction.KeyBinding.name, HotkeyButtonWidth)) {
 					currentAction = hotkeyAction;
 				}
 			}
@@ -94,25 +94,59 @@ namespace SASHotkeys
 			settingsWindowVisible = false;
 		}
 
+		private void CalculateKeyBindings()
+		{
+			var values = Enum.GetValues (typeof(KeyCode)).Cast<KeyCode> ();
+			allKeyBindings = new List<KeyBinding> ();
+			float maxWidth = 0.0f;
+			foreach (KeyCode key in values) {
+				String keyName = key.ToString ();
+				// JoystickButtonX refers to buttons for any joystick, so filter them out.
+				if (!keyName.StartsWith ("JoystickButton") && !keyName.StartsWith("Mouse")) {
+					allKeyBindings.Add (new KeyBinding (key));
+					maxWidth = Math.Max(maxWidth, GUI.skin.button.CalcSize(new GUIContent(keyName)).x);
+				}
+			}
+			hotkeyButtonWidth = GUILayout.Width(maxWidth + buttonWidthThreshold);
+		}
+
 		private List<KeyBinding> AllKeyBindings
 		{
 			get {
 				if (allKeyBindings == null) {
-					var values = Enum.GetValues (typeof(KeyCode)).Cast<KeyCode> ();
-					allKeyBindings = new List<KeyBinding> ();
-					foreach (KeyCode key in values) {
-						String keyName = key.ToString ();
-						// JoystickButtonX refers to buttons for any joystick, so filter them out.
-						if (!keyName.StartsWith ("JoystickButton") && !keyName.StartsWith("Mouse")) {
-							allKeyBindings.Add (new KeyBinding (key));
-						}
-					}
+					CalculateKeyBindings ();
 				}
 				return allKeyBindings;
 			}
 		}
+	
+		private GUILayoutOption HotkeyButtonWidth
+		{
+			get {
+				if (hotkeyButtonWidth == null) {
+					CalculateKeyBindings ();
+				}
+				return hotkeyButtonWidth;
+			}
+		}
+
+		private GUILayoutOption ClearButtonWidth
+		{
+			get {
+				if (clearButtonWidth == null) {
+					clearButtonWidth = GUILayout.Width(
+						GUI.skin.button.CalcSize (new GUIContent (clearButtonText)).x + buttonWidthThreshold);
+				}
+				return clearButtonWidth;
+			}
+		}
+
+		private const float buttonWidthThreshold = 10.0f;
+		private const String clearButtonText = "Clear";
 
 		private List<KeyBinding> allKeyBindings;
+		private GUILayoutOption hotkeyButtonWidth;
+		private GUILayoutOption clearButtonWidth;
 		private bool settingsWindowVisible = false;
 		private ApplicationLauncherButton toolbarButton;
 		private HotkeyAction currentAction;
